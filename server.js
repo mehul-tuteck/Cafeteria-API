58222d1072ccb9477c5995ca266678ef3bcdc530
mehul2015 <mehuldev2015@gmail.com>
2022-07-12 18:54:12 +0530

    HTML

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const cron = require("node-cron");
-const { sequelize, Sequelize } = require("sequelize");
-const nodemailer = require("nodemailer");
-const twilio = require("twilio");
+require('dotenv').config();
+const express = require('express');
+const cron = require('node-cron');
+const { sequelize, Sequelize } = require('sequelize');
+const nodemailer = require('nodemailer');
 
-const db = require("./dbSetup");
-const Users = require("./users")(db.sequelize, Sequelize);
-const router = require("./routes");
-const { transporter, mailOptions } = require("./mailConfig.js");
+const db = require('./dbSetup');
+const Users = require('./users')(db.sequelize, Sequelize);
+const router = require('./routes');
+const { transporter, mailOptions } = require('./mailConfig.js');
 
-const client = new twilio(process.env.SID, process.env.TOKEN);
+const sid = process.env.SID;
+const token = process.env.TOKEN;
+
+const client = require('twilio')(process.env.SID, process.env.TOKEN);
 
 const app = express();
 app.use(router);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log("Database connected");
+  console.log('Database connected');
   console.log(`Server started on port ${PORT}`);
 });
 
 //Running a script everyday at 11 PM to clear the DB.
-cron.schedule("* 0 23 * * *", async () => {
+cron.schedule('* 0 23 * * *', async () => {
   try {
     const users = await Users.findAll();
 
@@ -40,19 +42,19 @@ cron.schedule("* 0 23 * * *", async () => {
   } catch (error) {
     client.messages.create({
       body: `DB update failed with ${error.message}`,
-      to: "+917980996735",
+      to: '+917980996735',
       from: process.env.FROM,
     });
   }
 });
 
 //Running another script everyday at 7:30 PM to send a mail to Aritra Da on weekdays.
-cron.schedule("* 30 19 * * 1-5", async () => {
+cron.schedule('* 30 19 * * 1-5', async () => {
   transporter.sendMail(mailOptions, (error, response) => {
     if (error) {
       client.messages.create({
         body: `Mailing failed to Aritra with ${error}`,
-        to: "+917980996735",
+        to: '+917980996735',
         from: process.env.FROM,
       });
     }
