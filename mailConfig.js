2ed397124aafc86ed7f865f9cd3f9f19556f0e6c
mehul2015 <mehuldev2015@gmail.com>
2022-07-12 18:40:52 +0530

    HTML

--- mailConfig.js
+++ mailConfig.js
@@ -1,18 +1,18 @@
 const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.com",
+  host: 'smtp.zoho.com',
   secure: true,
   port: 465,
   auth: {
-    user: "mehul.chattopadhyay@tuteck.com",
+    user: 'mehul.chattopadhyay@tuteck.com',
     pass: process.env.EMAIL_PASSWORD,
   },
 });
 
 const mailOptions = {
-  from: "mehul.chattopadhyay@tuteck.com",
-  to: "aritra.mitra@tuteck.com",
+  from: 'mehul.chattopadhyay@tuteck.com',
+  to: 'aritra.mitra@tuteck.com',
   subject: `The Tuteck Cafeteria`,
-  html: <p>PFA the coffee details for today</p>,
+  html: '<p>PFA the coffee details for today</p>',
 };
 
 module.exports = {
