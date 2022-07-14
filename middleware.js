17ab6d4a74b102511233ad205b6b46f94f6554a5
mehul2015 <mehuldev2015@gmail.com>
2022-07-14 22:39:22 +0530

    API

--- middleware.js
+++ middleware.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(404).send("Access token not found");
+    return res.status(404).send('Access token not found');
   }
   const token = authHeader;
   try {
-    const employee = jwt.verify(token, "tuteck-caf");
-    req.phoneNumber = employee.phoneNumber;
+    const employee = jwt.verify(token, 'tuteck-caf');
+    req.email = employee.email;
     req.name = employee.name;
     next();
   } catch (error) {
-    return res.status(400).send("Auth token invalid");
+    return res.status(400).send('Auth token invalid');
   }
 };
 
