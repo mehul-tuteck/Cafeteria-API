185a279422aaf1347dcffde6669716da40462607
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-07-30 12:01:20 +0530

    Cron

--- utils/pingCron.js
+++ utils/pingCron.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const axios = require("axios");
 
 const ping = () => {
-  cron.schedule("* */20 * * * *", async () => {
+  cron.schedule("*/20 * * * *", async () => {
     const response = await axios.get("https://tuteck-caf.herokuapp.com/");
     console.log(response.data);
   });
